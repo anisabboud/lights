07ebc3941b6b84b67a95cadde43fb041d65afff7
Anis Abboud <anis.abboud@gmail.com>
2014-03-07 11:52:21 -0500

    solve functions

--- lights.ino
+++ lights.ino
@@ -5,6 +5,7 @@ int N = 3;
 
 bool level[N][N];
 bool state[N][N];
+bool solution[N][N];
 
 bool inBoard(int row, int col) {
 	return row >= 0 && row < N && col >= 0 && col < N;
@@ -40,10 +41,36 @@ void newLevel() {
 
 	for (int row = 0; row < N; row++) {
 		for (int col = 0; col < N; col++) {
-			level[row][col] = (randNumber % 2 == 1);
+			state[row][col] = level[row][col] = (randNumber % 2 == 1);
 			randNumber >>= 1;
 		}
 	}	
+}
+
+// Magic. Don't touch.
+void solveCorner(int row, int col) {
+	solution[row][col] = (!state[row][col] + !state[row][2 - col] + !state[2 - row][col] + !state[1][2 - col] + !state[2 - row][1]) % 2 == 1;
+}
+void solveEdge(int row, int col) {
+	solution[row][col] = (!state[1][1] + !state[2 - row][2 - col] + !state[2 - row][col] + !state[1][2 - col]) % 2 == 1;
+}
+void solveCenter() {
+	solution[1][1] = (!state[0][1] + !state[1][0] + !state[1][1] + !state[1][2] + !state[2][1]) % 2 == 1;
+}
+
+// Assumes N==3!
+void solve() {
+	solveCorner(0, 0);
+	solveCorner(0, 2);
+	solveCorner(2, 2);
+	solveCorner(2, 2);
+
+	solveEdge(0, 1);
+	solveEdge(1, 0);
+	solveEdge(1, 2);
+	solveEdge(2, 1);
+
+	solveCenter();
 }
 
 void setup() {
